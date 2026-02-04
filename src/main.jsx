069aa843bdc9a0282fbbf37053550e1ca1525a67
mothalbete <ivancarpinteroferreiro@gmail.com>
2026-02-04 17:00:49 +0100

    se han cambiado otras cosas

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
 import "bootstrap/dist/css/bootstrap.min.css";  // primero Bootstrap
-import "./index.css";                           // opcional, pero antes
 import "./App.css";                             // tus estilos, los últimos
 
 import { AuthProvider } from "./context/AuthContext.jsx";
